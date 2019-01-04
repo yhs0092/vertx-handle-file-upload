3241d295db7d02effbda58d1932c59e86a369ed4
yaohaishi <yaohaishi@huawei.com>
2019-01-04 16:01:00 +0800

    check problem in the case that uploaded file is too large

--- src/main/java/com/github/yhs0092/demo/vertx/AppMain.java
+++ src/main/java/com/github/yhs0092/demo/vertx/AppMain.java
@@ -3,7 +3,6 @@ package com.github.yhs0092.demo.vertx;
 import java.util.Set;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -21,36 +20,14 @@ public class AppMain {
 
     Router router = Router.router(vertx);
 
-    // Here I set uploads dir as null, but it doesn't work as I expect.
-//    router.route().handler(new RejectUploadsHandler());
-//        .handler(ctx -> {
-//          System.out.println("pre handle");
-//          ctx.request().uploadHandler(upload -> {
-////        ctx.request().connection().close();
-////        ctx.response()
-////            .putHeader("Content-Type", "text/plain")
-////            .end("uploading file not supported");
-//            System.out.println("heihei!");
-//          });
-//          ctx.next();
-//        })
-//        .handler(BodyHandler.create())
-//        .handler(ctx -> {
-//          System.out.println("go on");
-//          ctx.next();
-//        });
-
     router.route()
         .handler(
-            BodyHandler.create().setBodyLimit(256)/*.setHandleFileUploads(true)*/.setDeleteUploadedFilesOnEnd(true))
+            BodyHandler.create().setBodyLimit(256).setDeleteUploadedFilesOnEnd(true))
         .failureHandler(context -> {
-          LOGGER.info(
-              "get failure, context status [" + context.statusCode() + "], response status [" + context.response()
-                  .getStatusCode() + "]",
-              context.failure());
+          LOGGER.info("get failure!", context.failure());
 
           if (!context.response().ended()) {
-            context.response().setStatusCode(400).end("end!!");
+            context.response().setStatusCode(413).end("failure response");
           }
           if (!context.response().closed()) {
             LOGGER.info("close response");
@@ -68,13 +45,6 @@ public class AppMain {
       routingContext.response()
           .putHeader("Content-Type", "text/plain")
           .end("get " + fileUploads.size() + " files.");
-    });
-    router.routeWithRegex("/testPost").handler(routingContext -> {
-      final Buffer body = routingContext.getBody();
-      LOGGER.info("/testPost gets body: [" + body + "]");
-      routingContext.response()
-          .putHeader("Content-Type", routingContext.request().getHeader("Content-Type"))
-          .end(body.toString());
     });
 
     httpServer.requestHandler(router::accept).listen(8080,
